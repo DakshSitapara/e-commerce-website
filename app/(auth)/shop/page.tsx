8644a27c3a3157a8ab92145170341ac7ea425309
DakshSitapara <dakshsitapara@gmail.com>
2025-07-09 11:40:40 +0530

    improve UI

--- app/(auth)/shop/page.tsx
+++ app/(auth)/shop/page.tsx
@@ -2,31 +2,56 @@
 
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
-import { ShoppingCart, User, Star, Heart, Search, RotateCw } from "lucide-react";
-import { products, CategoryColor, TypeColor, Category, Type } from "@/lib/shop_data";
+import {
+  ShoppingCart,
+  User,
+  Star,
+  Heart,
+  Search,
+  RotateCw,
+} from "lucide-react";
+import {
+  products,
+  CategoryColor,
+  TypeColor,
+  Category,
+  Type,
+} from "@/lib/shop_data";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
-import {useCartStore} from "@/lib/cartStore";
-import {useWishlistStore} from "@/lib/wishlistStore";
-import {Input} from "@/components/ui/input";
-import {Select,SelectContent,SelectItem,SelectTrigger,SelectValue} from "@/components/ui/select";
-
+import { useCartStore } from "@/lib/cartStore";
+import { useWishlistStore } from "@/lib/wishlistStore";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function ShopPage() {
-  const { addToCart ,cart } = useCartStore();
-  const { wishlist, addToWishlist , removeFromWishlist } = useWishlistStore();
+  const { addToCart, cart, updateQuantity, removeFromCart } = useCartStore();
+  const { wishlist, addToWishlist, removeFromWishlist } = useWishlistStore();
   const router = useRouter();
   const [search, setSearch] = useState("");
   const [category, setCategory] = useState("all");
   const [type, setType] = useState("all");
   const [price, setPrice] = useState("all");
-  const filteredProducts = products.filter((product) =>
-    product.name.toLowerCase().includes(search.toLowerCase()) &&
-    (category === "all" || product.category === category) &&
-    (type === "all" || product.type === type)
+  const filteredProducts = products.filter(
+    (product) =>
+      product.name.toLowerCase().includes(search.toLowerCase()) &&
+      (category === "all" || product.category === category) &&
+      (type === "all" || product.type === type)
   );
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -38,8 +63,10 @@ export default function ShopPage() {
       <nav className="fixed top-0 z-10 w-full bg-white shadow-md">
         <div className="mx-auto max-w-8xl px-4 sm:px-6 lg:px-8">
           <div className="flex h-16 items-center justify-between">
-            <h1 className="text-2xl font-bold text-gray-900">Shop</h1>
-            <div className="flex items-center space-x-4">
+            <h1 className="text-xl sm:text-2xl font-bold text-gray-900">
+              Shop
+            </h1>
+            <div className="flex items-center space-x-2 sm:space-x-4">
               <div className="relative flex-1">
                 <Input
                   type="text"
@@ -57,7 +84,9 @@ export default function ShopPage() {
                 aria-label="Wishlist"
               >
                 <Heart className="h-5 w-5" />
-                Wishlist ({wishlist.length})
+                <span className="hidden sm:inline">
+                  Wishlist ({wishlist.length})
+                </span>
               </Button>
               <Button
                 variant="outline"
@@ -66,73 +95,71 @@ export default function ShopPage() {
                 aria-label="Cart"
               >
                 <ShoppingCart className="h-5 w-5" />
-                Cart ({cart.length})
+                <span className="hidden sm:inline">Cart ({cart.length})</span>
               </Button>
-              <Button variant="outline" className="flex items-center gap-2" onClick={() => router.push("/account")}>
+              <Button
+                variant="outline"
+                className="flex items-center gap-2"
+                onClick={() => router.push("/account")}
+              >
                 <User className="h-5 w-5" />
-                Account
+                <span className="hidden sm:inline">Account</span>
               </Button>
             </div>
           </div>
         </div>
       </nav>
       <main className="mx-auto max-w-8xl px-4 sm:px-6 lg:px-8 pt-20 pb-8">
-
-              <div className="flex flex-row gap-4 items-center mb-4">
-                <h2>Filtere : </h2>
-                <Select
-                  value={category}
-                  onValueChange={setCategory}
-                >
-                  <SelectTrigger className="flex items-center space-x-2">
-                    <span className="text-sm font-medium">Category:</span>
-                    <SelectValue />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="all">All</SelectItem>
-                    {Object.values(Category).map((category) => (
-                      <SelectItem key={category} value={category}>
-                        {category}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-                <Select
-                  value={type}
-                  onValueChange={setType}
-                >
-                  <SelectTrigger className="flex items-center space-x-2">
-                    <span className="text-sm font-medium">Type:</span>
-                    <SelectValue />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="all">All</SelectItem>
-                    {Object.values(Type).map((type) => (
-                      <SelectItem key={type} value={type}>
-                        {type}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-                <Button
-                  variant="ghost"
-                  className="flex items-center"
-                  onClick={() => {
-                    setSearch("");
-                    setCategory("all");
-                    setType("all");
-                  }}
-                >
-                  <RotateCw className="h-5 w-5"/>
-                </Button>
-              </div>
-
+        <div className="flex flex-col sm:flex-row gap-4 items-center mb-4">
+          <h2 className="text-sm sm:text-lg">Filter:</h2>
+          <div className="flex flex-col sm:flex-row gap-4 items-center w-full">
+            <Select value={category} onValueChange={setCategory}>
+              <SelectTrigger className="flex items-center space-x-2 w-full sm:w-auto">
+                <span className="text-sm font-medium">Category:</span>
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="all">All</SelectItem>
+                {Object.values(Category).map((category) => (
+                  <SelectItem key={category} value={category}>
+                    {category}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+            <Select value={type} onValueChange={setType}>
+              <SelectTrigger className="flex items-center space-x-2 w-full sm:w-auto">
+                <span className="text-sm font-medium">Type:</span>
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="all">All</SelectItem>
+                {Object.values(Type).map((type) => (
+                  <SelectItem key={type} value={type}>
+                    {type}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+            <Button
+              variant="ghost"
+              className="flex items-center w-full sm:w-auto"
+              onClick={() => {
+                setSearch("");
+                setCategory("all");
+                setType("all");
+              }}
+            >
+              <RotateCw className="h-5 w-5" />
+            </Button>
+          </div>
+        </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          {filteredProducts.length > 0 ? (
+          {filteredProducts.length > 0 ? (  
             filteredProducts.map((product) => (
               <Card
                 key={product.id}
-                className="relative w-70 group overflow-hidden transition-all hover:shadow-xl py-0"
+                className="relative w-full sm:w-70 group overflow-hidden transition-all hover:shadow-xl py-0"
               >
                 <CardHeader className="p-0">
                   <div className="relative h-full w-full">
@@ -141,12 +168,37 @@ export default function ShopPage() {
                       alt={product.name}
                       width={500}
                       height={500}
-                      className="h-80 w-100 transition-transform group-hover:scale-105"
+                      className="h-80 w-full transition-transform group-hover:scale-105"
                     />
+                    <Button
+                    title={
+                      wishlist.some((item) => item.id === product.id)
+                        ? "Remove from wishlist"
+                        : "Add to wishlist"
+                    }
+                      className="absolute top-2 right-2 bg-transparent shadow-none hover:shadow-none hover:bg-transparent"
+                      onClick={() => {
+                        if (wishlist.some((item) => item.id === product.id)) {
+                          removeFromWishlist(product.id);
+                          toast.success(`${product.name} removed from wishlist!`);
+                        } else {
+                          addToWishlist(product);
+                          toast.success(`${product.name} added to wishlist!`);
+                        }
+                      }}
+                      hidden={cart.some((item) => item.id === product.id)}
+                    >
+                      <Heart
+                        className={`h-6 w-6 ${wishlist.some((item) => item.id === product.id)
+                          ? "fill-red-500 text-red-500"
+                          : " text-red-500"}
+                        `}
+                      />
+                    </Button>
                   </div>
                 </CardHeader>
                 <CardContent className="p-4 py-0 space-y-2">
-                  <CardTitle className="text-lg font-semibold">
+                  <CardTitle className="text-sm sm:text-lg font-semibold">
                     {product.name}
                   </CardTitle>
                   <div className="flex items-center justify-between">
@@ -167,6 +219,13 @@ export default function ShopPage() {
                 </CardContent>
                 <CardFooter className="flex flex-col items-center justify-center p-4 pt-0">
                   <Button
+                  variant={
+                    cart.some((item) => item.id === product.id)
+                      ? "destructive"
+                      : wishlist.some((item) => item.id === product.id)
+                      ? "outline"
+                      : "outline"
+                  }
                     className="w-full"
                     onClick={() => {
                       if (wishlist.some((item) => item.id === product.id)) {
@@ -174,31 +233,25 @@ export default function ShopPage() {
                         addToCart(product);
                         toast.success(`${product.name} added to cart!`);
                         return;
+                      }else if (cart.some((item) => item.id === product.id)) {
+                        removeFromCart(product.id);
+                        toast.success(`${product.name} removed from cart!`);
+                        return;
                       }
                       addToCart(product);
                       toast.success(`${product.name} added to cart!`);
                     }}
                     aria-label="Add to Cart"
-                    disabled={cart.some((item) => item.id === product.id)}
                   >
-                    Add to Cart
+                    {cart.some((item) => item.id === product.id)
+                      ? "Remove from Cart"
+                      : "Add to Cart"}
                   </Button>
-                  <Button
-                    className="w-full mt-2"
-                    onClick={() => {
-                      addToWishlist(product);
-                      toast.success(`${product.name} added to wishlist!`);
-                    }}
-                    aria-label="Add to Wishlist"
-                    disabled={wishlist.some((item) => item.id === product.id) || cart.some((item) => item.id === product.id)}
-                  >
-                    Add to Wishlist
-                  </Button>
-                </CardFooter>
+               </CardFooter>
               </Card>
             ))
           ) : (
-            <div className=" font-black text-2xl text-center">
+            <div className=" font-black text-xl sm:text-2xl text-center">
               No products found.
             </div>
           )}
@@ -207,4 +260,3 @@ export default function ShopPage() {
     </div>
   );
 }
-
